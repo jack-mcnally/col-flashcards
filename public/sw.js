f8e906f4395aa7440edfb9ad28410a302a4ce098
Jack McNally <jackmcnally@MacBookPro.modem>
2026-04-09 00:44:26 +1000

    Configure GitHub Pages deployment with base path

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'col-flashcards-v1'
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) =>
-      cache.addAll(['/', '/index.html'])
+      cache.addAll(['/col-flashcards/', '/col-flashcards/index.html'])
     )
   )
   self.skipWaiting()
